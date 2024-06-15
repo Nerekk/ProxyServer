a693fe1b9fe83ef48b593d149b2c4cd9de444df4
Nerekk <arkadiusz.salaj.02@gmail.com>
2024-06-15 22:57:15 +0200

    Config exception rename

---
+++ src/main/java/org/example/proxyserver/Proxy/Exceptions/ConfigException.java
@@ -0,0 +1,7 @@
+package org.example.proxyserver.Proxy.Exceptions;
+
+public class ConfigException extends Exception {
+    public ConfigException(String message) {
+        super(message);
+    }
+}
--- src/main/java/org/example/proxyserver/Proxy/Exceptions/ValidationException.java
+++
@@ -1,7 +0,0 @@
-package org.example.proxyserver.Proxy.Exceptions;
-
-public class ValidationException extends Exception {
-    public ValidationException(String message) {
-        super(message);
-    }
-}
