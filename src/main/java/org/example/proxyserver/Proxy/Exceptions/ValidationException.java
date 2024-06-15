57b5888464176fb24d965f5db3d3113b968d88cb
Nerekk <arkadiusz.salaj.02@gmail.com>
2024-06-15 20:59:57 +0200

    Proxy config exception

---
+++ src/main/java/org/example/proxyserver/Proxy/Exceptions/ValidationException.java
@@ -0,0 +1,7 @@
+package org.example.proxyserver.Proxy.Exceptions;
+
+public class ValidationException extends Exception {
+    public ValidationException(String message) {
+        super(message);
+    }
+}
