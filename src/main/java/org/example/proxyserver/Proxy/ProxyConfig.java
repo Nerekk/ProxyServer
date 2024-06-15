18f1730d3ddd9db2f9fc4f914c79e676a2104a72
Nerekk <arkadiusz.salaj.02@gmail.com>
2024-06-15 22:57:33 +0200

    ProxyConfig getters implementation

--- src/main/java/org/example/proxyserver/Proxy/ProxyConfig.java
+++ src/main/java/org/example/proxyserver/Proxy/ProxyConfig.java
@@ -1,7 +1,8 @@
 package org.example.proxyserver.Proxy;
 
 
-import org.example.proxyserver.Proxy.Exceptions.ValidationException;
+import org.example.proxyserver.Proxy.Exceptions.ConfigException;
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.io.IOException;
@@ -22,24 +23,69 @@ public class ProxyConfig {
         this.jsonData = jsonData;
     }
 
-    public static ProxyConfig getConfig(String filename) throws IOException, ValidationException {
+    public static ProxyConfig getConfig(String filename) throws IOException, ConfigException {
         String content = new String(Files.readAllBytes(Paths.get(filename)));
 
         JSONObject json = new JSONObject(content);
-        validate(json);
-        return new ProxyConfig(json);
+        ProxyConfig config = new ProxyConfig(json);
+
+        validate(config);
+
+        return config;
+    }
+
+    private static void validate(ProxyConfig config) throws ConfigException {
+        String[] fields = {SERVER_ID, LISTEN_ADDRESSES, LISTEN_PORT, TIME_OUT, SIZE_LIMIT, ALLOWED_IP_ADDRESSES};
+        ConfigValidator.validateFields(fields, config);
+    }
+
+    public JSONObject getJsonData() {
+        return jsonData;
     }
 
     public Object getValue(String key) {
         return jsonData.get(key);
     }
 
-    private static void validate(JSONObject jsonObject) throws ValidationException {
-        String[] fields = {SERVER_ID, LISTEN_ADDRESSES, LISTEN_PORT, TIME_OUT, SIZE_LIMIT, ALLOWED_IP_ADDRESSES};
-
-        for (String field : fields) {
-            if (!jsonObject.has(field))
-                throw new ValidationException(field + " is missing.");
-        }
+    public String getServerId() {
+        return jsonData.getString(SERVER_ID);
     }
+
+    public int getListenedPort() {
+        return jsonData.getInt(LISTEN_PORT);
+    }
+
+    public int getTimeOut() {
+        return jsonData.getInt(TIME_OUT);
+    }
+
+    public int getSizeLimit() {
+        return jsonData.getInt(SIZE_LIMIT);
+    }
+
+    public String[] getListenAddresses() {
+        JSONArray jsonArray = jsonData.getJSONArray(LISTEN_ADDRESSES);
+
+        String[] addresses = parseToStringArray(jsonArray);
+
+        return addresses;
+    }
+
+    public String[] getAllowedAddresses() {
+        JSONArray jsonArray = jsonData.getJSONArray(ALLOWED_IP_ADDRESSES);
+
+        String[] addresses = parseToStringArray(jsonArray);
+
+        return addresses;
+    }
+
+    private static String[] parseToStringArray(JSONArray jsonArray) {
+        String[] addresses = new String[jsonArray.length()];
+
+        for (int i = 0; i < jsonArray.length(); i++) {
+            addresses[i] = jsonArray.getString(i);
+        }
+        return addresses;
+    }
+
 }
