126a9f84fd77daaf3bf0af56c88afdfc0dd17bb2
Nerekk <arkadiusz.salaj.02@gmail.com>
2024-06-23 23:11:44 +0200

    Server user input implementation

--- src/main/java/org/example/proxyserver/FXController.java
+++ src/main/java/org/example/proxyserver/FXController.java
@@ -2,9 +2,10 @@ package org.example.proxyserver;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import org.example.proxyserver.Proxy.Exceptions.ConfigException;
-import org.example.proxyserver.Proxy.ProxyConfig;
+import javafx.scene.control.TextArea;
+import org.example.proxyserver.Gui.Logger;
 import org.example.proxyserver.Proxy.ProxyServerManager;
 
 import java.io.IOException;
@@ -13,25 +14,57 @@ import java.util.ResourceBundle;
 
 public class FXController implements Initializable {
     @FXML
-    private Label welcomeText;
+    private TextArea topicsText, loggerText, serversText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
+    private Button startButton, stopButton, refreshButton;
 
-    private ProxyConfig config;
+    public Logger logger;
+
+    private ProxyServerManager manager;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        this.logger = new Logger(topicsText, loggerText, serversText);
+        Logger.setInstance(this.logger);
+    }
+
+    @FXML
+    protected void start() {
+        loggerText.clear();
+        logger.log(Logger.INFO, "Starting main server..");
         try {
-            config = ProxyConfig.getConfig();
-
-            ProxyServerManager manager = new ProxyServerManager(this, config);
-
+            manager = new ProxyServerManager(this);
             manager.run();
         } catch (IOException e) {
             throw new RuntimeException(e);
+        }
+
+        swapButtonStatus();
+    }
+
+    @FXML
+    protected void stop() {
+        manager.stop();
+        logger.log(Logger.INFO, "Main server stopped.");
+
+        swapButtonStatus();
+    }
+
+    @FXML
+    protected void refresh() {
+        manager.refreshGui();
+    }
+
+    private void swapButtonStatus() {
+        if (startButton.isDisabled()) {
+            startButton.setDisable(false);
+            stopButton.setDisable(true);
+            refreshButton.setDisable(true);
+        } else {
+            startButton.setDisable(true);
+            stopButton.setDisable(false);
+            refreshButton.setDisable(false);
         }
     }
 }
