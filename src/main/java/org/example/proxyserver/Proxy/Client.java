b10b2e4d197e99d9cd34a04341d49f8bc547b86f
Nerekk <arkadiusz.salaj.02@gmail.com>
2024-06-24 23:24:12 +0200

    Client class implementation

---
+++ src/main/java/org/example/proxyserver/Proxy/Client.java
@@ -0,0 +1,24 @@
+package org.example.proxyserver.Proxy;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.*;
+import java.net.Socket;
+
+@Getter
+@Setter
+public class Client {
+    // TODO ustawianie userId w odpowiednim momencie
+    private String userId;
+
+    private Socket clientSocket;
+    private InputStream inputStream;
+    private OutputStream outputStream;
+
+    public Client(Socket clientSocket) throws IOException {
+        this.clientSocket = clientSocket;
+        this.inputStream = clientSocket.getInputStream();
+        this.outputStream = clientSocket.getOutputStream();
+    }
+}
