6825f945bfbeb9023943660e42d99457ea4e12f3
Nerekk <arkadiusz.salaj.02@gmail.com>
2024-06-24 23:28:57 +0200

    ClientHandler thread implementation

--- src/main/java/org/example/proxyserver/Threads/ClientHandler.java
+++ src/main/java/org/example/proxyserver/Threads/ClientHandler.java
@@ -1,32 +1,44 @@
 package org.example.proxyserver.Threads;
 
+import org.example.proxyserver.Proxy.Client;
+import org.example.proxyserver.Proxy.Com_resources.MessageReceived;
+import org.example.proxyserver.Proxy.Com_resources.ProxyResources;
+
 import java.io.DataInputStream;
-import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public class ClientHandler extends Thread {
-    private Socket clientSocket;
-    private DataInputStream inputStream;
-    private DataOutputStream outputStream;
+    private Client client;
+    private DataInputStream in;
+    private final ProxyResources resources;
 
-    private volatile boolean STOP_CLIENT;
+    private final AtomicBoolean STOP_CLIENT;
 
-    public ClientHandler(Socket clientSocket, boolean STOP_CLIENT) throws IOException {
-        this.clientSocket = clientSocket;
-        this.inputStream = new DataInputStream(clientSocket.getInputStream());
-        this.outputStream = new DataOutputStream(clientSocket.getOutputStream());
-
+    public ClientHandler(Socket clientSocket, ProxyResources resources, AtomicBoolean STOP_CLIENT) throws IOException {
+        this.client = new Client(clientSocket);
+        this.resources = resources;
         this.STOP_CLIENT = STOP_CLIENT;
 
+        this.in = new DataInputStream(this.client.getInputStream());
     }
 
     public void run() {
-        System.out.println("### [" + clientSocket.getInetAddress() + "] Handling client..");
+        System.out.println("### [" + client.getClientSocket().getInetAddress() + "] Handling client..");
 
-        while (!STOP_CLIENT) {
+        while (!STOP_CLIENT.get()) {
+            String jsonData;
+            try {
+                jsonData = in.readUTF();
+                System.out.println(jsonData);
+            } catch (IOException _) {
+                continue;
+            }
 
+            if (!jsonData.isEmpty())
+                resources.addMessageReceived(new MessageReceived(client, jsonData));
         }
-
+        System.out.println("### [" + client.getClientSocket().getInetAddress() + "] Handling ended");
     }
 }
